75712c30aaa5c3e75b2cc951bc85434f0bb098c7
Rakeshreddy-07 <rakeshreddynarra07@gmail.com>
2025-04-10 18:12:22 +0530

    jenkins

---
+++ vars/pipelinesGlobals.groovy
@@ -0,0 +1,12 @@
+def getAccountID(String environment){
+    switch(environment) { 
+        case 'dev': 
+            return "361769572747"
+        case 'qa':
+            return "361769572747"
+        case 'prod':
+            return "361769572747"
+        default:
+            return "nothing"
+    } 
+}
