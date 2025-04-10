d7acd34af44830c585f031a6341eaf27feeb18f2
Rakeshreddy-07 <rakeshreddynarra07@gmail.com>
2025-04-10 17:57:46 +0530

    jenkins

--- vars/nodeJSEKSPipeline.groovy
+++ vars/nodeJSEKSPipeline.groovy
@@ -53,23 +53,30 @@ def call(Map configMap){
             when{
                 expression {params.deploy}
             }
+            steps {
+                withAWS(region: 'us-east-1', credentials: 'aws-creds-terraform'){
+                
+                sh """
+                aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-east-1.amazonaws.com
+
+                docker build -t ${account_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/${component}:${appVersion} .
+
+                 docker images
+
+                docker push ${account_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/${component}:${appVersion}
+               
+                """
+            }
+            }
+        }
+        stage('Deploy'){
             steps{
+                steps{
                 build job: 'backend-cd', parameters: [
                     string(name: 'version', value: "$appVersion"),
                     string(name: 'ENVIRONMENT', value: "dev"),
                     ], wait: true
             }
-        }
-        stage('Deploy'){
-            steps{
-                withAWS(region: 'us-east-1', credentials: 'aws-creds-terraform'){
-                    sh """
-                       aws eks update-kubeconfig --region ${region} --name ${project}-${environment}  
-                       cd helm
-                       sed -i 's/IMAGE_VERSION/${appVersion}/g' values-${environment}.yaml
-                       helm upgrade --install ${component} -n ${project} -f values-${environment}.yaml .
-                    """
-                }
 
             }
         }
